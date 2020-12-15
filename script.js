4301a5cf957db6ac0816d589735b21e6617c2918
Glitch (new-da-vinci-site) <none>
2020-12-15 17:17:47 +0000

    🚀📍 Updated with Glitch

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
+function myFunction() {
+  document.getElementById("fontsize").style.fontSize = "x-large";
+}
